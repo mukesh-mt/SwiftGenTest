8f89006e25fe2c6437dd468962ab17a9e380c19e
Mukesh Yadav <mukesh.yadav@mindtickle.com>
2022-12-26 17:36:57 +0530

    Initial commit

---
+++ SwiftGenTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftGenTest
+//
+//  Created by Mukesh Yadav on 26/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
